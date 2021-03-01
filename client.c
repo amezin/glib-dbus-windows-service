879404feb61877d0af0ad0e61e7ebc22ed26b244
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-03-01 14:24:03 +0600

    D-Bus client and server

--- client.c
+++ client.c
@@ -1,4 +1,59 @@
-int main(void)
+#include <stdlib.h>
+
+#include <gio/gio.h>
+
+#include "test-dbus-interface.h"
+
+int main(int argc, char *argv[])
 {
-    return 0;
+    g_autoptr(GError) error = NULL;
+
+    g_autoptr(GFile) address_file = g_file_new_for_path("dbus-server-address");
+    g_autoptr(GBytes) address = g_file_load_bytes(address_file, NULL, NULL, &error);
+    if (!address) {
+        g_warning("Can't read server address file: %s", error->message);
+        return EXIT_FAILURE;
+    }
+
+    g_message("Connecting to %s", (const char *)g_bytes_get_data(address, NULL));
+
+    g_autoptr(GDBusConnection) connection = g_dbus_connection_new_for_address_sync(
+        g_bytes_get_data(address, NULL),
+        G_DBUS_CONNECTION_FLAGS_AUTHENTICATION_CLIENT,
+        NULL,
+        NULL,
+        &error
+    );
+
+    if (!connection) {
+        g_warning("Can't create D-Bus connection: %s", error->message);
+        return EXIT_FAILURE;
+    }
+
+    g_message("Connected");
+
+    g_autoptr(TestPeerInterface) iface = test_peer_interface_proxy_new_sync(
+        connection,
+        G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES | G_DBUS_PROXY_FLAGS_DO_NOT_CONNECT_SIGNALS,
+        NULL,
+        "/",
+        NULL,
+        &error
+    );
+
+    if (!iface) {
+        g_warning("Can't create D-Bus interface: %s", error->message);
+        return EXIT_FAILURE;
+    }
+
+    g_message("Created interface");
+
+    g_autofree gchar *response = NULL;
+    if (!test_peer_interface_call_hello_world_sync(iface, "test", &response, NULL, &error)) {
+        g_warning("Method call failed: %s", error->message);
+        return EXIT_FAILURE;
+    }
+
+    g_print("Response: %s\n", response);
+    return EXIT_SUCCESS;
 }
--- service.c
+++ service.c
@@ -1,4 +1,82 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include <gio/gio.h>
+
+#include "test-dbus-interface.h"
+
+static gboolean handle_hello_world(TestPeerInterface *iface,
+                                   GDBusMethodInvocation *invocation,
+                                   const gchar *greeting,
+                                   gpointer user_data G_GNUC_UNUSED)
+{
+    g_message("Received request: %s", greeting);
+    test_peer_interface_complete_hello_world(iface, invocation, greeting);
+    return TRUE;
+}
+
+static gboolean handle_connection(GDBusServer *server,
+                                  GDBusConnection *connection,
+                                  gpointer user_data)
+{
+    GDBusInterfaceSkeleton *skeleton = G_DBUS_INTERFACE_SKELETON(user_data);
+    g_autoptr(GError) error = NULL;
+
+    if (g_dbus_interface_skeleton_export(skeleton, connection, "/", &error))
+        return TRUE;
+
+    g_warning("Can't export D-Bus interface: %s", error->message);
+    return FALSE;
+}
+
 int main(void)
 {
-    return 0;
+    g_autoptr(GError) error = NULL;
+
+    g_autoptr(TestPeerInterface) skeleton = test_peer_interface_skeleton_new();
+    g_signal_connect(skeleton, "handle-hello-world", G_CALLBACK(handle_hello_world), NULL);
+
+    g_autofree gchar *guid = g_dbus_generate_guid();
+    g_autoptr(GDBusServer) server = g_dbus_server_new_sync(
+        "tcp:host=localhost",
+        G_DBUS_SERVER_FLAGS_AUTHENTICATION_ALLOW_ANONYMOUS,
+        guid,
+        NULL,
+        NULL,
+        &error
+    );
+
+    if (!server) {
+        g_warning("Can't create D-Bus server: %s", error->message);
+        return EXIT_FAILURE;
+    }
+
+    g_signal_connect_object(server, "new-connection", G_CALLBACK(handle_connection), skeleton, 0);
+    g_dbus_server_start(server);
+
+    const gchar *client_address = g_dbus_server_get_client_address(server);
+    g_message("D-Bus server is listening on %s", client_address);
+
+    g_autoptr(GFile) address_file = g_file_new_for_path("dbus-server-address");
+    gboolean file_written = g_file_replace_contents(
+        address_file,
+        client_address,
+        strlen(client_address),
+        NULL,
+        FALSE,
+        G_FILE_CREATE_REPLACE_DESTINATION,
+        NULL,
+        NULL,
+        &error
+    );
+
+    if (!file_written) {
+        g_warning("Can't write server address to file: %s", error->message);
+        return EXIT_FAILURE;
+    }
+
+    g_autoptr(GMainLoop) loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(loop);
+
+    return EXIT_SUCCESS;
 }
